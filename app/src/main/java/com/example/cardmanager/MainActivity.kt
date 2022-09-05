f03ee645a35c2604ce2cbecaef87c63aebcd0111
Lucas Emanuel Oliveira Xavier <CIANDT\lucaseox@lnb035155cps.ciandt.global>
2022-09-05 11:02:02 -0300

    setup fragment login and nav

--- app/src/main/java/com/example/cardmanager/MainActivity.kt
+++ app/src/main/java/com/example/cardmanager/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.cardmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cardmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(R.style.Theme_CardManager)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
